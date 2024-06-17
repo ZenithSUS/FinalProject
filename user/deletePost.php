72f128d68f69828b13eefe24d7ea677e58299cfc
ZenithSUS <jeranmerino147@gmail.com>
2024-06-17 14:09:38 +0800

    Fix bugs

--- user/deletePost.php
+++ user/deletePost.php
@@ -2,6 +2,12 @@
     //Include queries
     include "../queries/post.php";
 
+    //Include db connection
+    include "../db.php";
+
+    //Intialize session
+    session_start();
+
     //Check if user is logged in
     if(!isset($_SESSION['user_id'])) {
         header("Location: ../auth/login.php");
